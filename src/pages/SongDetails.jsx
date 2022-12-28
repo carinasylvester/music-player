46ad08c9b778852234e1e5af81ea9e2887aa58e1
Carina Sylvester <csylvester@vultr.com>
2022-12-28 10:30:47 -0600

    added song display & card

--- src/pages/SongDetails.jsx
+++ src/pages/SongDetails.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useSelector, useDispatch } from 'react-redux';
 import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
-
 import { setActiveSong, playPause } from '../redux/features/playerSlice';
 import { useGetSongDetailsQuery, useGetSongRelatedQuery } from '../redux/services/shazamCore';
 
