5f993ee17bec792e94a1d89d4b3b0e46ab40db33
Carina Sylvester <csylvester@vultr.com>
2022-12-28 10:31:23 -0600

    implemented search & search bar

--- src/components/Searchbar.jsx
+++ src/components/Searchbar.jsx
@@ -1,5 +1,38 @@
-const Searchbar = () => (
-  <div>Loader</div>
-);
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+
+import { FiSearch } from 'react-icons/fi';
+
+const Searchbar = () => {
+  const navigate = useNavigate();
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    navigate(`/search/${searchTerm}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} autoComplete="off" className="p-2 text-yellow-800 focus-within:text-yellow-600">
+      <label htmlFor="search-field" className="sr-only">
+        Search all files
+      </label>
+      <div className="flex flex-row justify-start items-center">
+        <FiSearch aria-hidden="true" className="w-5 h-10 ml-4" />
+        <input
+          name="search-field"
+          autoComplete="off"
+          id="search-field"
+          className="flex-1 bg-transparent border-none placeholder-yellow-800 outline-none text-base text-yellow p-4"
+          placeholder="What do you want to hear?"
+          type="search"
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+      </div>
+    </form>
+  );
+};
 
 export default Searchbar;
